090b1ffc0775f50d2c6a6bcb0ba6264e37898d5d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2026-03-03 20:46:02 +0100

    Limpieza

--- docker/scripts/reset_terraform.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd /terraform || exit
-
-rm -rf .terraform
-rm -f .terraform.lock.hcl
-rm -f terraform.tfstate
-rm -f terraform.tfstate.backup
--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    opennebula = {
-      source  = "OpenNebula/opennebula"
-      version = "~> 1.4"
-    }
-  }
-}
